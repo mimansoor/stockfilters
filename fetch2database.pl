99e7f4d3d48244e9f6b1cd66a0f441b10a9679b4
Illyas Mansoor <mimansoor@gmail.com>
2017-02-13 22:45:05 +0530

    squash! More modifications made
    Signed-off-by: Illyas Mansoor <mimansoor@gmail.com>

--- fetch2database.pl
+++ fetch2database.pl
@@ -1,14 +1,11 @@
 #/usr/bin/perl
 
 use lib qw( ..);
-use JSON;
-use LWP::Simple;
 use DBI;
+use DateTime qw(:all);
 
-my $url = "http://www.google.com/finance/info?infotype=infoquoteall&q=NSE:NIFTY, NSE:GRASIM, NSE:TECHM, NSE:TCS, NSE:INFY, NSE:ADANIPORTS, NSE:BANKBARODA, NSE:INDUSINDBK, NSE:TATASTEEL, NSE:NTPC, NSE:KOTAKBANK, NSE:HCLTECH, NSE:COALINDIA, NSE:HDFCBANK, NSE:AXISBANK, NSE:TATAPOWER, NSE:EICHERMOT, NSE:LT, NSE:BAJAJ-AUTO, NSE:ONGC, NSE:BHARTIARTL, NSE:BHEL, NSE:YESBANK, NSE:WIPRO, NSE:ACC, NSE:SUNPHARMA, NSE:ULTRACEMCO, NSE:HEROMOTOCO, NSE:HINDUNILVR, NSE:AMBUJACEM, NSE:SBIN, NSE:RELIANCE, NSE:IDEA, NSE:HDFC, NSE:TATAMTRDVR, NSE:HINDALCO, NSE:MARUTI, NSE:TATAMOTORS, NSE:INFRATEL, NSE:ICICIBANK, NSE:ZEEL, NSE:ASIANPAINT, NSE:BOSCHLTD, NSE:POWERGRID, NSE:ITC, NSE:CIPLA, NSE:GAIL, NSE:DRREDDY, NSE:LUPIN, NSE:BPCL, NSE:AUROPHARMA, NSE:NIFTYJR, NSE:MCDOWELL-N, NSE:BAJAJFINSV, NSE:ABB, NSE:MOTHERSUMI, NSE:BAJFINANCE, NSE:SRTRANSFIN, NSE:CONCOR, NSE:PIDILITIND, NSE:OFSS, NSE:COLPAL, NSE:TITAN, NSE:MARICO, NSE:TORNTPHARM, NSE:OIL, NSE:GLAXO, NSE:BRITANNIA, NSE:PNB, NSE:EMAMILTD, NSE:DLF, NSE:PEL, NSE:SHREECEM, NSE:CASTROLIND, NSE:HAVELLS, NSE:GODREJCP, NSE:PFC, NSE:IBULHSGFIN, NSE:GSKCONS, NSE:DABUR, NSE:GLENMARK, NSE:JSWSTEEL, NSE:VEDL, NSE:LICHSGFIN, NSE:CADILAHC, NSE:BHARATFORG, NSE:PGHH, NSE:SIEMENS, NSE:APOLLOHOSP, NSE:HINDPETRO, NSE:DIVISLAB, NSE:INDIGO, NSE:BEL, NSE:NMDC, NSE:UPL, NSE:UBL, NSE:CUMMINSIND, NSE:HINDZINC, NSE:IOC, NSE:BANKNIFTY";
-
-#my $header = "time,open,high,low,close,prevclose,change,perchange,volume,hi52,lo52,\n";
-#print $header;
+use strict;
+use warnings;
 
 my $driver   = "SQLite";
 my $database = "intraday_data.db";
@@ -19,67 +16,58 @@ my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
                       or die $DBI::errstr;
 print "Opened database successfully\n";
 
-$id = 1;
 
-while (1) {
-	my $json_str = get($url);
-	die "Couldn't get $url" unless defined $json_str;
 
-	$json_str =~ s/\/\///;
+open (my $fh, '<', 'Company_names.lst') or die "Could not open file 'Company_names.lst' $!";
 
-	my $data;
-
-	$data = decode_json( $json_str );
-	foreach my $item (@{$data}) {
-	   	#print "$item->{'ltt'},$item->{'op'},$item->{'hi'},$item->{'lo'},$item->{'l'},$item->{'vo'}\n";
-		undef $stmt;
-		my $name = $item->{'t'};
-		$name =~ s/,//;
-		my $open = $item->{'op'};
-		$open =~ s/,//;
-		my $high = $item->{'hi'};
-		$high =~ s/,//;
-		my $low  = $item->{'lo'};
-		$low =~ s/,//;
-		my $last = $item->{'l_fix'};
-		$last =~ s/,//;
-		my $prev_close = $item->{'pcls_fix'};
-		$prev_close =~ s/,//;
-		my $change = $item->{'c_fix'};
-		$change =~ s/,//;
-		my $change_per = $item->{'cp_fix'};
-		$change_per =~ s/,//;
-		my $volume = $item->{'vo'};
-		$volume =~ s/-/0/;
-		$volume =~ s/m/M/;
-		$volume =~ s/k/K/;
-		$volume =~ s/,//;
-		$volume =~ /.*(M)/;
-		if ($1 eq "M") {
-			$volume =~ s/M//g;
-			$volume = $volume * 1000000;
+my $data;
+my $filename;
+my $full_time=0;
+my $id=1;
+while (my $row = <$fh>) {
+	chomp $row;
+	my $stock_name = $row;
+	$filename = qq($stock_name.txt);
+	open(my $fh1, '<', $filename) or die "Could not open file '$filename' $!";
+	my $line;
+	$line = <$fh1>; #Remove EXCHANGE line
+	$line = <$fh1>; #Remove MARKET_OPEN_MINUTE
+	$line = <$fh1>; #Remove MARKET_CLOSE_MINUTE
+	$line = <$fh1>; #Remove INTERVAL=60
+	$line =~ /INTERVAL=(.*)/;
+	my $interval = $1;
+	$line = <$fh1>; #Remove COLUMNS
+	$line = <$fh1>; #Remove DATA=
+	$line = <$fh1>; #Remove TIMEZONE_OFFSET
+	$line =~ /TIMEZONE_OFFSET=(.*)/;
+	my $timezone_offset = $1;
+	while ($line = <$fh1>) {
+		# Get the tokens first
+		my @tokens = split/,/, $line;
+		my $time = $tokens[0];
+		if ($time =~ /a.*/) {
+			$time =~ /a(.*)/;
+			$full_time = $1;
+			$time =~ s/a//;
 		} else {
-			$volume =~ /.*(K)/;
-			if ($1 eq "K") {
-				$volume =~ s/K//g;
-				$volume = $volume * 1000;
-			}	
+			$time = $full_time + $interval*$time;
 		}
-		my $hi52 = $item->{'hi52'};
-		$hi52 =~ s/,//;
-		my $lo52 = $item->{'lo52'};
-		$lo52 =~ s/,//;
 
-		my $time = $item->{'lt_dts'};
-		my $date;
-		$time =~ /(.*)T(.*)Z/;
+		$time += $timezone_offset*60;
+
+		my $dt = DateTime->from_epoch( epoch => $time);
+		$dt =~ /(.*)T(.*)/;
 		$time = $2;
-		$date = $1;
-		my $stmt = qq(INSERT INTO INTRADAY_DATA (ID,NAME,OPEN,HIGH,LOW,LAST,PREVCLOSE,CHANGE,PERCHANGE,VOLUME,HI52,LO52,TIME,DATE)
-VALUES ($id, '$name', $open, $high, $low, $last, $prev_close, $change, $change_per, $volume, $hi52, $lo52, '$time', '$date'));
+		my $date = $1;
+
+		my $stmt = qq(INSERT INTO INTRADAY_GETPRICES_BACKFILL (ID,DATE,TIME,NAME,CLOSE,HIGH,LOW,OPEN,VOLUME)
+VALUES ($id, '$date', '$time', '$stock_name', $tokens[1], $tokens[2], $tokens[3], $tokens[4], $tokens[5]));
 		my $rv = $dbh->do($stmt) or die $DBI::errstr;
+
 		$id++;
 	}
 
-	sleep 60;
+	close $fh1;
 }
+
+$dbh->disconnect();
--- intraday_realtime_get.pl
+++ intraday_realtime_get.pl
@@ -9,20 +9,28 @@ use DateTime qw(:all);
 sub check_for_download_time {
 	my $time_now = DateTime->now( time_zone => 'local' );
 	my $download = 1;
+	my $dow = $time_now->day_of_week;
 
-	#Start only after 9:15AM
-	#Stop after 15:30PM
-	if ($time_now->hour < 9 || $time_now->hour > 15) {
+	#Do not download on Saturday's and Sunday's
+	#Saturday == 6, Sunday == 7
+	if ($dow == 6 || $dow == 7) {
 		$download = 0;
 	} else {
-		if ($time_now->hour == 9) {
-			if ($time_now->minute < 15) {
-				$download = 0;
-			}
+		#On Week Day's ie., Monday to Friday
+		#Start only after 9:15AM
+		#Stop after 15:30PM
+		if ($time_now->hour < 9 || $time_now->hour > 15) {
+			$download = 0;
 		} else {
-			if ($time_now->hour == 15) {
-				if ($time_now->minute > 30) {
+			if ($time_now->hour == 9) {
+				if ($time_now->minute < 15) {
 					$download = 0;
+				}
+			} else {
+				if ($time_now->hour == 15) {
+					if ($time_now->minute > 30) {
+						$download = 0;
+					}
 				}
 			}
 		}
@@ -31,7 +39,7 @@ sub check_for_download_time {
 	return $download;
 }
 
-my $url = "http://www.google.com/finance/info?infotype=infoquoteall&q=NSE:NIFTY, NSE:GRASIM, NSE:TECHM, NSE:TCS, NSE:INFY, NSE:ADANIPORTS, NSE:BANKBARODA, NSE:INDUSINDBK, NSE:TATASTEEL, NSE:NTPC, NSE:KOTAKBANK, NSE:HCLTECH, NSE:COALINDIA, NSE:HDFCBANK, NSE:AXISBANK, NSE:TATAPOWER, NSE:EICHERMOT, NSE:LT, NSE:BAJAJ-AUTO, NSE:ONGC, NSE:BHARTIARTL, NSE:BHEL, NSE:YESBANK, NSE:WIPRO, NSE:ACC, NSE:SUNPHARMA, NSE:ULTRACEMCO, NSE:HEROMOTOCO, NSE:HINDUNILVR, NSE:AMBUJACEM, NSE:SBIN, NSE:RELIANCE, NSE:IDEA, NSE:HDFC, NSE:TATAMTRDVR, NSE:HINDALCO, NSE:MARUTI, NSE:TATAMOTORS, NSE:INFRATEL, NSE:ICICIBANK, NSE:ZEEL, NSE:ASIANPAINT, NSE:BOSCHLTD, NSE:POWERGRID, NSE:ITC, NSE:CIPLA, NSE:GAIL, NSE:DRREDDY, NSE:LUPIN, NSE:BPCL, NSE:AUROPHARMA, NSE:NIFTYJR, NSE:MCDOWELL-N, NSE:BAJAJFINSV, NSE:ABB, NSE:MOTHERSUMI, NSE:BAJFINANCE, NSE:SRTRANSFIN, NSE:CONCOR, NSE:PIDILITIND, NSE:OFSS, NSE:COLPAL, NSE:TITAN, NSE:MARICO, NSE:TORNTPHARM, NSE:OIL, NSE:GLAXO, NSE:BRITANNIA, NSE:PNB, NSE:EMAMILTD, NSE:DLF, NSE:PEL, NSE:SHREECEM, NSE:CASTROLIND, NSE:HAVELLS, NSE:GODREJCP, NSE:PFC, NSE:IBULHSGFIN, NSE:GSKCONS, NSE:DABUR, NSE:GLENMARK, NSE:JSWSTEEL, NSE:VEDL, NSE:LICHSGFIN, NSE:CADILAHC, NSE:BHARATFORG, NSE:PGHH, NSE:SIEMENS, NSE:APOLLOHOSP, NSE:HINDPETRO, NSE:DIVISLAB, NSE:INDIGO, NSE:BEL, NSE:NMDC, NSE:UPL, NSE:UBL, NSE:CUMMINSIND, NSE:HINDZINC, NSE:IOC, NSE:BANKNIFTY";
+my $url = "http://www.google.com/finance/info?infotype=infoquoteall&q=NSE:NIFTY, NSE:GRASIM, NSE:TECHM, NSE:TCS, NSE:INFY, NSE:ADANIPORTS, NSE:BANKBARODA, NSE:INDUSINDBK, NSE:TATASTEEL, NSE:NTPC, NSE:KOTAKBANK, NSE:HCLTECH, NSE:COALINDIA, NSE:HDFCBANK, NSE:AXISBANK, NSE:TATAPOWER, NSE:TAKE, NSE:LT, NSE:BAJAJ-AUTO, NSE:ONGC, NSE:BHARTIARTL, NSE:BHEL, NSE:YESBANK, NSE:WIPRO, NSE:ACC, NSE:SUNPHARMA, NSE:KRBL, NSE:HEROMOTOCO, NSE:HINDUNILVR, NSE:AMBUJACEM, NSE:SBIN, NSE:RELIANCE, NSE:IDEA, NSE:HDFC, NSE:TATAMTRDVR, NSE:HINDALCO, NSE:PENIND, NSE:TATAMOTORS, NSE:INFRATEL, NSE:ICICIBANK, NSE:ZEEL, NSE:ASIANPAINT, NSE:RAYMOND, NSE:POWERGRID, NSE:ITC, NSE:CIPLA, NSE:GAIL, NSE:DRREDDY, NSE:LUPIN, NSE:BPCL, NSE:AUROPHARMA, NSE:NIFTYJR, NSE:MCDOWELL-N, NSE:BAJAJFINSV, NSE:ABB, NSE:MOTHERSUMI, NSE:BAJFINANCE, NSE:SRTRANSFIN, NSE:CONCOR, NSE:PIDILITIND, NSE:OFSS, NSE:COLPAL, NSE:TITAN, NSE:MARICO, NSE:TORNTPHARM, NSE:OIL, NSE:GLAXO, NSE:BRITANNIA, NSE:PNB, NSE:EMAMILTD, NSE:DLF, NSE:PEL, NSE:JISLJALEQS, NSE:CASTROLIND, NSE:HAVELLS, NSE:GODREJCP, NSE:PFC, NSE:IBULHSGFIN, NSE:TALWALKARS, NSE:DABUR, NSE:GLENMARK, NSE:JSWSTEEL, NSE:VEDL, NSE:LICHSGFIN, NSE:CADILAHC, NSE:BHARATFORG, NSE:TV18BRDCST, NSE:SIEMENS, NSE:APOLLOHOSP, NSE:HINDPETRO, NSE:DIVISLAB, NSE:INDIGO, NSE:BEL, NSE:NMDC, NSE:UPL, NSE:UBL, NSE:CUMMINSIND, NSE:HINDZINC, NSE:IOC, NSE:BANKNIFTY";
 
 
 system("cp realtime_data.db tmp_abcdefgh.db");
@@ -42,9 +50,10 @@ my $dsn = "DBI:$driver:dbname=$database";
 my $userid = "";
 my $password = "";
 
-$id = 101;
+$id = 1;
 
 while (1) {
+	my $start_time = time();
 	if (check_for_download_time()) {
 		my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
 				      or die $DBI::errstr;
@@ -102,7 +111,7 @@ while (1) {
 			$time = $2;
 			$date = $1;
 			my $stmt = qq(INSERT INTO INTRADAY_DATA (ID,NAME,OPEN,HIGH,LOW,LAST,PREVCLOSE,CHANGE,PERCHANGE,VOLUME,HI52,LO52,TIME,DATE)
-	VALUES ($id, '$name', $open, $high, $low, $last, $prev_close, $change, $change_per, $volume, $hi52, $lo52, '$time', '$date'));
+				VALUES ($id, '$name', $open, $high, $low, $last, $prev_close, $change, $change_per, $volume, $hi52, $lo52, '$time', '$date'));
 			my $rv = $dbh->do($stmt) or die $DBI::errstr;
 			$id++;
 		}
@@ -111,6 +120,11 @@ while (1) {
 		system("cp tmp_abcdefgh.db realtime_data.db");
 	}
 
-	sleep 60;
+	my $end_time = time();
+	my $diff_time = ($end_time - $start_time);
+	my $delay_in_seconds = 60;
+	my $sleep_time = $delay_in_seconds - $diff_time - $end_time%$delay_in_seconds;
+	my $time_now = DateTime->now( time_zone => 'local' );
+	sleep $sleep_time;
 }
 
